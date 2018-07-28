ba61561e7e2f7f31d7d0e839d5cec4a572696386
Leonardo Felipe Nerone <leonardofelipenerone@gmail.com>
2018-07-28 16:47:34 -0300

    chore: update docz configuration

--- doczrc.js
+++ doczrc.js
@@ -2,7 +2,9 @@ import { babel } from 'docz-plugin-babel6'
 import { css } from 'docz-plugin-css'
 
 export default {
-  dest: './docs',
+  title: 'react-img-zoom',
+  description: 'A React component to zoom images on hover',
+  dest: '/docs',
   themeConfig: {
     mode: 'dark',
   },
